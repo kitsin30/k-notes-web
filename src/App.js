53b11209562ca813e9c26459f645b2697bfdd88a
kitsin30 <sinkit123@gmail.com>
2024-05-04 23:02:13 +0700

    update loginform

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import './App.css';
 import LoginForm from './components/loginForm/LoginForm';
+import Home from './components/home/Home';
+
+const PrivateRoute = () => {
+  const user = JSON.parse(localStorage.getItem('username'));
+  return user ? <Outlet /> : <Navigate to="/" replace />;
+}
 
 function App() {
   return (
     <div>
-      <LoginForm />
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<LoginForm />} />
+          <Route element={<PrivateRoute />} >
+            <Route path='/home' element={<Home />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './LoginForm.css';
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+
+    e.preventDefault()
+    console.log(username);
+
+    try {
+      const response = await fetch('http://localhost:8080/user/findbyid', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if(data.status === 200){
+        localStorage.setItem('username', JSON.stringify(data.obj.username));
+        localStorage.setItem('id', JSON.stringify(data.obj.id));
+        navigate("/home");
+        console.log("true");
+        
+      } else{
+        console.log("false");
+      }
+
+    } catch (error) {
+      console.error(error);
+      // Handle login error
+    }
+
+    
+  }
+
   return (
     <div className='wrapper'>
-      <form action="">
+      <form onSubmit={handleLogin}>
         <h1>Login</h1>
         <div className='input-box'>
-          <input type="text" placeholder='Username' name="" id="" required />
+          <input type="text" placeholder='Username' id="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
           <FaUser className='icon'/>
         </div>
 
         <div className='input-box'>
-          <input type="password" placeholder='Password' name="" id="" required />
+          <input type="password" placeholder='Password' id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           <FaLock className='icon'/>
         </div>
 
